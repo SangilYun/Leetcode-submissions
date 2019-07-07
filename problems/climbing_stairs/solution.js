6437da43abd8ea069d0723c4ca0bcf0d383795f7
sangil <nasuler@gmail.com>
2019-07-07 12:29:33 +0000

    Sync LeetCode submission - Climbing Stairs (javascript)

--- problems/climbing_stairs/solution.js
+++ problems/climbing_stairs/solution.js
@@ -3,12 +3,16 @@
  * @return {number}
  */
 var climbStairs = function(n) {
-  
-    let dp = [];
-    dp.push(1);
-    dp.push(1);
-    for(let i=2; i<=n; i++){
-        dp[i] = dp[i-1] +dp[i-2];
+    let arr = [];
+    //0, 1 -> return 1
+    if(n <=1) return 1;
+    else{
+        arr.push(1);
+        arr.push(1);        
+        for(let i=2; i<=n; i++){
+            arr[i] = arr[i-1]+arr[i-2];
+        }
     }
-    return dp[n];
+    return arr[n];
+
 };
