c7b351e8e5e24ebb29c9f05c40adc61b2873c2b0
sangil <nasuler@gmail.com>
2019-07-28 06:48:35 +0000

    Sync LeetCode submission - House Robber (javascript)

--- problems/house_robber/solution.js
+++ problems/house_robber/solution.js
@@ -5,23 +5,20 @@
 var rob = function(nums) {
     if(nums.length===0){
         return 0;
-    }else if(nums.length===1){
-        return nums[0];
-    }else if(nums.length===2){
-        return Math.max(nums[0], nums[1]);
-    }else{
-        let dp = Array(nums.length);
-        dp[0] = nums[0];
-        dp[1] = Math.max(nums[0], nums[1]);
-        for(let i=2; i<dp.length; i++){
-            dp[i] = Math.max(dp[i-2] + nums[i], dp[i-1]);
-        }
-        return dp[nums.length-1]
     }
+    if(nums.length ===1){
+        return nums[0];
+    }
+    if(nums.length === 2){
+        return Math.max(nums[0], nums[1]);
+    }
+    
+    let dp = new Array(nums.length).fill(0);
+    dp[0] = nums[0];
+    dp[1] = Math.max(nums[0], nums[1]);
+    for(let i=2; i<nums.length; i++){
+        dp[i] = Math.max(nums[i] + dp[i-2], dp[i-1]);
+    }
+    return dp[dp.length-1];
+    
 };
-
-//[1, 2, 5, 2, 9, 99, 77]
-
-//2+2+99 = 103
-//1+5+9+77 = 92
-//1+5+99 = 105
