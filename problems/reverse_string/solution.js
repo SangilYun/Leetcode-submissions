76fd50f4d93c9bd955ff0f1e5a56e9fe3621a1fe
sangil <nasuler@gmail.com>
2019-08-11 05:11:18 +0000

    Sync LeetCode submission - Reverse String (javascript)

--- problems/reverse_string/solution.js
+++ problems/reverse_string/solution.js
@@ -3,10 +3,12 @@
  * @return {void} Do not return anything, modify s in-place instead.
  */
 var reverseString = function(s) {
-    for(let i=0; i<s.length/2;i++){
-        let temp = s[i];
-        s[i] = s[s.length-1-i];
-        s[s.length-1-i] = temp;
+    let left =0;
+    let right = s.length-1;
+    
+    while(left < right){
+        [s[left], s[right]] = [s[right], s[left]];
+        left ++;
+        right --;
     }
-    return s;
 };
