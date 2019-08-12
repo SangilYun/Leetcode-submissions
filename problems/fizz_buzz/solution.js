e5b3c9b4938e8a3de6e813e14ce15078a98fedd9
sangil <nasuler@gmail.com>
2019-08-12 09:36:33 +0000

    Sync LeetCode submission - Fizz Buzz (javascript)

--- problems/fizz_buzz/solution.js
+++ problems/fizz_buzz/solution.js
@@ -3,19 +3,16 @@
  * @return {string[]}
  */
 var fizzBuzz = function(n) {
-    let arr=[];
+    let arr = [];
     for(let i=1; i<=n; i++){
-        if(i%15===0){
+        if(i % 15 === 0 ){
             arr.push("FizzBuzz");
-        }
-        else if(i%3===0){
-            arr.push("Fizz");
-        }
-        else if(i%5===0){
-            arr.push("Buzz");
-        }
-        else{
-            arr.push(i.toString());
+        }else if(i%3 === 0){
+            arr.push('Fizz')
+        }else if(i % 5 === 0 ){
+            arr.push('Buzz');
+        }else{
+            arr.push(""+i);
         }
     }
     return arr;
