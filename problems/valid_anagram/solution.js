893f31bf3fc62d7aa96e3a4633181ce17269b49a
sangil <nasuler@gmail.com>
2019-08-23 12:26:30 +0000

    Sync LeetCode submission - Valid Anagram (javascript)

--- problems/valid_anagram/solution.js
+++ problems/valid_anagram/solution.js
@@ -6,17 +6,15 @@
 var isAnagram = function(s, t) {
     if(s.length!==t.length) return false;
     let table = new Array(26).fill(0);
-    let offSet = 'a'.charCodeAt(0)
+    const offset = 'a'.charCodeAt();
     for(let i=0; i<s.length; i++){
-        table[s.charCodeAt(i)-offSet]++;
-        table[t.charCodeAt(i)-offSet]--;
-        
+        let sIndex = s[i].charCodeAt() - offset;
+        let tIndex = t[i].charCodeAt() - offset;
+        table[sIndex] += 1;
+        table[tIndex] -= 1;
     }
+
+    return table.filter(each => each!==0).length>0 ? false:true;
     
-    for(let i=0; i<table.length; i++){
-        if(table[i]!==0){
-            return false;
-        }
-    }
-    return true;
+    
 };
