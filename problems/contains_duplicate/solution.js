ac68f17844316458a392243886a79dca282596a6
sangil <nasuler@gmail.com>
2019-08-23 12:36:40 +0000

    Sync LeetCode submission - Contains Duplicate (javascript)

--- problems/contains_duplicate/solution.js
+++ problems/contains_duplicate/solution.js
@@ -3,5 +3,11 @@
  * @return {boolean}
  */
 var containsDuplicate = function(nums) {
-    return new Set(nums).size < nums.length;
+    return new Set(nums).size !== nums.length;
 };
+
+/*
+1100 xor
+1100
+0000
+*/
