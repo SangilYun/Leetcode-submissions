d53c067c18546eb10fa1885789eca35eae00f204
sangil <nasuler@gmail.com>
2019-08-24 04:01:42 +0000

    Sync LeetCode submission - Intersection of Two Arrays II (javascript)

--- problems/intersection_of_two_arrays_ii/solution.js
+++ problems/intersection_of_two_arrays_ii/solution.js
@@ -4,12 +4,11 @@
  * @return {number[]}
  */
 var intersect = function(nums1, nums2) {
-    
-    let table = nums1.reduce((table, curr)=>{
-        table[curr] = table[curr]+1 ||1;
-        return table;
-    },{})
-    
+    let table = nums1.reduce((acc, curr) => {
+        acc[curr] = acc[curr]+1 || 1;
+        return acc;
+    }, {})
+        
     return nums2.filter(each => {
         if(table[each]){
             table[each]--;
