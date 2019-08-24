68e77c6132eb05f9fa25a519af00db4f7be3bb26
sangil <nasuler@gmail.com>
2019-08-24 07:57:39 +0000

    Sync LeetCode submission - Merge Two Sorted Lists (javascript)

--- problems/merge_two_sorted_lists/solution.js
+++ problems/merge_two_sorted_lists/solution.js
@@ -11,13 +11,19 @@
  * @return {ListNode}
  */
 var mergeTwoLists = function(l1, l2) {
-    if(l1 == null) return l2;
-    if(l2 == null) return l1;
-    if(l1.val < l2.val){
-        l1.next = mergeTwoLists(l1.next, l2);
-        return l1;
-    } else{
-        l2.next = mergeTwoLists(l1, l2.next);
-        return l2;
+    let head =new ListNode(null);
+    let currNode = head;
+    while(l1 && l2){
+        if(l1.val > l2.val){
+            currNode.next = l2;
+            l2 = l2.next;
+        }else{
+            currNode.next = l1;
+            l1= l1.next;
+        }
+        currNode = currNode.next;
     }
+    
+    currNode.next = l1 || l2;
+    return head.next;
 };
