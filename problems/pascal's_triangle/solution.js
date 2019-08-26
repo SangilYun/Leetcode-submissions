41f097a67daf08ae7cffcee3c1d8a7fc11988a13
sangil <nasuler@gmail.com>
2019-08-26 15:24:42 +0000

    Sync LeetCode submission - Pascal's Triangle (javascript)

--- problems/pascal's_triangle/solution.js
+++ problems/pascal's_triangle/solution.js
@@ -3,16 +3,17 @@
  * @return {number[][]}
  */
 var generate = function(numRows) {
-    let pa = [];
+    let pascal = [];
     for(let i=0; i<numRows; i++){
-        pa.push([]);
+        pascal.push([]);
         for(let j=0; j<=i; j++){
-            if(j===0 || j===i){
-                pa[i][j]=1;
+            if(j===0 || j ===i){
+                pascal[i][j] = 1;
             }else{
-                pa[i][j] = pa[i-1][j-1] + pa[i-1][j];
+                pascal[i][j] = pascal[i-1][j-1] + pascal[i-1][j];
             }
         }
     }
-    return pa;
+    
+    return pascal;
 };
