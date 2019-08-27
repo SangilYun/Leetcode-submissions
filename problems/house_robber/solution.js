df4f7e1a7030b7c4c9a8fc60ce2193dbaf5443f6
sangil <nasuler@gmail.com>
2019-08-27 13:41:59 +0000

    Sync LeetCode submission - House Robber (javascript)

--- problems/house_robber/solution.js
+++ problems/house_robber/solution.js
@@ -3,22 +3,18 @@
  * @return {number}
  */
 var rob = function(nums) {
-    if(nums.length===0){
-        return 0;
+    if(nums.length===0) return 0;
+    if(nums.length===1) return nums[0];
+    let dp = [];
+    for(let i=0; i<nums.length; i++){
+        if(i<2){
+            dp.push(nums[i]);
+        }else if(i === 2){
+            dp.push(nums[i] + dp[i-2]);
+        }
+        else{
+            dp.push(Math.max(nums[i]+ dp[i-2], nums[i]+ dp[i-3]))
+        }
     }
-    if(nums.length ===1){
-        return nums[0];
-    }
-    if(nums.length === 2){
-        return Math.max(nums[0], nums[1]);
-    }
-    
-    let dp = new Array(nums.length).fill(0);
-    dp[0] = nums[0];
-    dp[1] = Math.max(nums[0], nums[1]);
-    for(let i=2; i<nums.length; i++){
-        dp[i] = Math.max(nums[i] + dp[i-2], dp[i-1]);
-    }
-    return dp[dp.length-1];
-    
+    return Math.max(dp[dp.length-1], dp[dp.length-2]);
 };
