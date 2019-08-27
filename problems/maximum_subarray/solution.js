fd45ead5d28c4f945bd58596d29529dec5d22ab6
sangil <nasuler@gmail.com>
2019-08-27 12:15:30 +0000

    Sync LeetCode submission - Maximum Subarray (javascript)

--- problems/maximum_subarray/solution.js
+++ problems/maximum_subarray/solution.js
@@ -3,11 +3,13 @@
  * @return {number}
  */
 var maxSubArray = function(nums) {
-    let sum =-Number.MAX_VALUE;
-    let temp =-Number.MAX_VALUE;
+    let max = -Number.MAX_VALUE;
+    
+    let temp = -Number.MAX_VALUE;
     for(let i=0; i<nums.length; i++){
-        temp= Math.max(temp+nums[i], nums[i])
-        sum = Math.max(temp, sum);
-    }
-    return sum;
+        temp = Math.max(temp+ nums[i], nums[i]);
+        max = temp > max ? temp : max;
+    }   
+    return max;
 };
+
