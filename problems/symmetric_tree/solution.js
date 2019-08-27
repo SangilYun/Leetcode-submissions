2697670fa0922d004bea56624d6617dc3125bb61
sangil <nasuler@gmail.com>
2019-08-27 10:00:06 +0000

    Sync LeetCode submission - Symmetric Tree (javascript)

--- problems/symmetric_tree/solution.js
+++ problems/symmetric_tree/solution.js
@@ -10,13 +10,13 @@
  * @return {boolean}
  */
 var isSymmetric = function(root) {
-    return isMirror(root, root)
+    return isMirror(root, root);
 };
 
-function isMirror(t1, t2){
-    if(t1===null && t2===null) return true;
-    if(t1===null || t2===null) return false;
-    return (t1.val === t2.val) 
-        &&isMirror(t1.right, t2.left)
-        &&isMirror(t1.left, t2.right);
+const isMirror = (lRoot, rRoot) =>{
+    if(!lRoot && !rRoot) return true;
+    if(!lRoot|| !rRoot) return false;
+    return lRoot.val === rRoot.val 
+        && isMirror(lRoot.right, rRoot.left)
+        && isMirror(lRoot.left, rRoot.right);
 }
