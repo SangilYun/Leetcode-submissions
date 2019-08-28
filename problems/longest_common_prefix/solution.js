6dccac95ad1d563d7b7570d0f091112d23293dd6
sangil <nasuler@gmail.com>
2019-08-28 00:46:34 +0000

    Sync LeetCode submission - Longest Common Prefix (javascript)

--- problems/longest_common_prefix/solution.js
+++ problems/longest_common_prefix/solution.js
@@ -2,15 +2,29 @@
  * @param {string[]} strs
  * @return {string}
  */
+
 var longestCommonPrefix = function(strs) {
-    if(!strs.length) return "";
-    
-    for(let i=0; i<strs[0].length; i++){
-        for(str of strs){
-            if( str[i] !== strs[0][i]){
-                return strs[0].slice(0,i);
-            }
+    if(strs.length === 0) return "";
+
+    for(let index=0; index<strs[0].length; index++){
+        let temp = strs[0][index];
+        for(let i=1; i<strs.length; i++){        
+            let str = strs[i];
+            if(str[index] !== temp) return str.slice(0, index);
         }
     }
     return strs[0];
 };
+
+// var longestCommonPrefix = function(strs) {
+//     if(!strs.length) return "";
+    
+//     for(let i=0; i<strs[0].length; i++){
+//         for(str of strs){
+//             if( str[i] !== strs[0][i]){
+//                 return strs[0].slice(0,i);
+//             }
+//         }
+//     }
+//     return strs[0];
+// };
