953f394715957cff22196409767d0b63bd539c42
sangil <nasuler@gmail.com>
2019-08-28 00:09:03 +0000

    Sync LeetCode submission - Merge Sorted Array (javascript)

--- problems/merge_sorted_array/solution.js
+++ problems/merge_sorted_array/solution.js
@@ -8,14 +8,14 @@
 var merge = function(nums1, m, nums2, n) {
     m--;
     n--;
-    while(m+n >= -1){
-        if(nums1[m] > nums2[n] || n<0){
-            nums1[m+n+1] = nums1[m];
-            m--;
-        }else{
-            nums1[m+n+1] = nums2[n];
-            n--;
-        }
+    while(m >= 0 && n>= 0){
+        if(nums1[m] > nums2[n]) nums1[m+n+1] = nums1[m--];
+        else nums1[m+n+1] = nums2[n--];
     }
     
+    while(n>=0){
+        nums1[m+n+1] = nums2[n--];
+    }
+    
+    return nums1;
 };
