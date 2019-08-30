cdf4c5c940d6e41d3a714835aeea3f038203be21
sangil <nasuler@gmail.com>
2019-08-30 12:27:51 +0000

    Sync LeetCode submission - Convert Sorted Array to Binary Search Tree (javascript)

--- problems/convert_sorted_array_to_binary_search_tree/solution.js
+++ problems/convert_sorted_array_to_binary_search_tree/solution.js
@@ -11,9 +11,10 @@
  */
 var sortedArrayToBST = function(nums) {
     if(!nums.length) return null;
-    let mid = Math.floor(nums.length/2);
-    let root = new TreeNode(nums[mid]);
-    root.left = sortedArrayToBST(nums.slice(0, mid));
-    root.right = sortedArrayToBST(nums.slice(mid+1))    
+    let m = Math.floor(nums.length/2);
+    let root = new TreeNode(nums[m]);
+    root.left = sortedArrayToBST(nums.slice(0, m));
+    root.right = sortedArrayToBST(nums.slice(m+1));
     return root;
+    
 };
