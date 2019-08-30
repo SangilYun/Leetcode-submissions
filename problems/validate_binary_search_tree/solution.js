903cd51f43c2040516ccb8f93775ba27f116ef85
sangil <nasuler@gmail.com>
2019-08-30 16:20:11 +0000

    Sync LeetCode submission - Validate Binary Search Tree (javascript)

--- problems/validate_binary_search_tree/solution.js
+++ problems/validate_binary_search_tree/solution.js
@@ -10,27 +10,31 @@
  * @return {boolean}
  */
 var isValidBST = function(root) {
-    let dfsList = dfs(root, []);
-    console.log('dfsList', dfsList);
-    for(let i=0; i<dfsList.length-1; i++){
-        if(dfsList[i] >= dfsList[i+1]){
-            return false;
-        }
+    return helper(root, -Infinity, Infinity);
+};
+
+function helper(root, min, max){
+    if(!root) return true;
+    if(root.val <= min || root.val >= max) return false;
+    if(!helper(root.left, min, root.val) || !helper(root.right, root.val, max)) return false;
+    
+    return true;
+}
+
+/*
+var isValidBST = function(root) {
+    let list =[];
+    helper(root, list);
+    for(let i=0; i<list.length-1; i++){
+        if(list[i] >= list[i+1]) return false;
     }
     return true;
 };
 
-function dfs(root, list){
-    if(!root){
-        return false;
-    }
-    if(root.left){
-        dfs(root.left, list)
-    }
+function helper(root, list){
+    if(!root) return null;
+    helper(root.left, list);
     list.push(root.val);
-    if(root.right){
-        dfs(root.right, list)
-    }
-    return list;
+    helper(root.right, list);
 }
-    
+*/
