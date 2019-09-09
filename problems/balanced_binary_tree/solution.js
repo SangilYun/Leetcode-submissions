1476e9cf61df7dc848e34686ba33c84f5cd62619
sangil <nasuler@gmail.com>
2019-09-09 03:19:24 +0000

    Sync LeetCode submission - Balanced Binary Tree (javascript)

--- problems/balanced_binary_tree/solution.js
+++ problems/balanced_binary_tree/solution.js
@@ -10,29 +10,6 @@
  * @return {boolean}
  */
 var isBalanced = function(root) {
-    return helper(root) !== -1;
-};
-
-function helper(root){
-    if(!root) return 0;
-    let leftHeight = helper(root.left);
-    if(leftHeight === -1) return -1;
-    
-    let rightHeight = helper(root.right);
-    if(rightHeight === -1) return -1;
-    
-    let heightDiff = Math.abs(leftHeight - rightHeight);
-    
-    if(heightDiff > 1){
-        return -1
-    }else{
-        return Math.max(leftHeight, rightHeight) +1;
-    }
-}
-
-
-/*
-var isBalanced = function(root) {
     if(!root) return true;
     let height = helper(root.left) - helper(root.right);
     return Math.abs(height) >1 ? false : isBalanced(root.left) && isBalanced(root.right);
@@ -42,4 +19,3 @@ function helper(root){
     if(!root) return 0;
     return Math.max(helper(root.left), helper(root.right)) +1;
 }
-*/
