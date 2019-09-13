db948f7e4876e3d3f424105bce32bd94de35afbb
sangil <nasuler@gmail.com>
2019-09-13 05:21:48 +0000

    Sync LeetCode submission - Best Time to Buy and Sell Stock II (javascript)

--- problems/best_time_to_buy_and_sell_stock_ii/solution.js
+++ problems/best_time_to_buy_and_sell_stock_ii/solution.js
@@ -3,9 +3,10 @@
  * @return {number}
  */
 var maxProfit = function(prices) {
-    let max = 0;
-    for(let i=1; i<prices.length; i++){
-        max += Math.max(0, prices[i]-prices[i-1]);
+    
+    let profit =0;
+    for(let i=0; i<prices.length-1; i++){
+        profit += Math.max(0, prices[i+1] - prices[i]);
     }
-    return max;
+    return profit;
 };
