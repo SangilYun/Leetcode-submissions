83e0676ef05f3078d9152269aebb5f833a3eb2df
sangil <nasuler@gmail.com>
2019-09-17 07:21:08 +0000

    Sync LeetCode submission - Move Zeroes (javascript)

--- problems/move_zeroes/solution.js
+++ problems/move_zeroes/solution.js
@@ -3,13 +3,14 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var moveZeroes = function(nums) {
-    let lastNonZeroFoundAt = 0;
+    let nonZeroIndex = 0;
     for(let i=0; i<nums.length; i++){
-        if(nums[i] !==0){
-            nums[lastNonZeroFoundAt++] = nums[i];
+        if(nums[i]!==0){
+            nums[nonZeroIndex++] = nums[i];
         }
     }
-    for(let i=lastNonZeroFoundAt; i<nums.length; i++){
+    for(let i=nonZeroIndex; i<nums.length; i++){
         nums[i] = 0;
     }
+    return nums;
 }
