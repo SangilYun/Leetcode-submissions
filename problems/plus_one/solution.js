f9a75a93648742697d6523d1aee9a59dc5bdc270
sangil <nasuler@gmail.com>
2019-09-17 07:14:47 +0000

    Sync LeetCode submission - Plus One (javascript)

--- problems/plus_one/solution.js
+++ problems/plus_one/solution.js
@@ -3,16 +3,15 @@
  * @return {number[]}
  */
 var plusOne = function(digits) {
-    for(let i=digits.length-1; i >= 0; i--){
+    
+    for(let i=digits.length-1; i>=0; i--){
         if(digits[i] < 9){
-            digits[i] ++;
+            digits[i]++;
             return digits;
         }
         digits[i] = 0;
     }
-    
     let arr = new Array(digits.length+1).fill(0);
-    arr[0]=1;
+    arr[0] = 1;
     return arr;
-    
 };
