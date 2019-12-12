be9c2c05ae4c304f84f2ec58993886fd8128b620
sangil <nasuler@gmail.com>
2019-12-12 03:09:12 +0000

    Sync LeetCode submission - Permutations (javascript)

--- problems/permutations/solution.js
+++ problems/permutations/solution.js
@@ -3,27 +3,24 @@
  * @return {number[][]}
  */
 var permute = function(nums) {
-    if(!nums) return null;
-    
-    let permutations = [];
-    if(nums.length === 0){
-        permutations.push([]);
-        return permutations;
-    }
-    
-    let first = nums[0];
-    let words = permute(nums.slice(1));
-    for(let word of words){
-        for(let i=0; i<= word.length; i++){
-            if(word.length===0) permutations.push([first]);
-            else{
-                permutations.push(insertAt(word, first, i));
-            }
-        }
-    }
-    return permutations
+    let result = [];
+    helper(result, nums, []);
+    return result;
 };
 
-function insertAt(arr, element, index){
-    return arr.slice(0, index).concat(element).concat(arr.slice(index));
+const helper = (result, nums, curr) =>{
+    if(nums.length ===curr.length){
+        result.push(curr.slice());
+        return;
+    }
+    
+    
+    for(let i=0; i<nums.length; i++){
+        let num = nums[i]
+        if(curr.indexOf(num) === -1){
+            curr.push(num);    
+            helper(result, nums, curr);
+            curr.pop();
+        }
+    }
 }
