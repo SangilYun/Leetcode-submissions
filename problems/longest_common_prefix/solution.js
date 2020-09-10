aa4abda2c3f0f4ec26e26a600e97b0417dc21291
sangil <nasuler@gmail.com>
2020-09-10 13:25:28 +0000

    Sync LeetCode submission - Longest Common Prefix (javascript)

--- problems/longest_common_prefix/solution.js
+++ problems/longest_common_prefix/solution.js
@@ -2,29 +2,27 @@
  * @param {string[]} strs
  * @return {string}
  */
-
 var longestCommonPrefix = function(strs) {
-    if(strs.length === 0) return "";
-
-    for(let index=0; index<strs[0].length; index++){
-        let temp = strs[0][index];
-        for(let i=1; i<strs.length; i++){        
-            let str = strs[i];
-            if(str[index] !== temp) return str.slice(0, index);
-        }
+    if(strs.length ===0){
+        return '';
     }
-    return strs[0];
-};
-
-// var longestCommonPrefix = function(strs) {
-//     if(!strs.length) return "";
+    if(strs.length === 1){
+        return strs[0];
+    }
+    let result = '';
+    let index = 0;
+    for(let i=0; i<strs[0].length; i++){
+        const currentChar = strs[0][i]
+        for(let j=1; j<strs.length; j++){
+            const currentString = strs[j];
+            const charToCompare = currentString[index];
+            if(currentChar !== charToCompare ){
+                return result;
+            }
+        }
+        result += currentChar;
+        index ++;
+    }
+    return result;
     
-//     for(let i=0; i<strs[0].length; i++){
-//         for(str of strs){
-//             if( str[i] !== strs[0][i]){
-//                 return strs[0].slice(0,i);
-//             }
-//         }
-//     }
-//     return strs[0];
-// };
+};
