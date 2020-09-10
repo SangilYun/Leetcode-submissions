84a0da5e297cb117782deecd762119ecc786f8aa
sangil <nasuler@gmail.com>
2020-09-10 13:58:07 +0000

    Sync LeetCode submission - Two Sum (javascript)

--- problems/two_sum/solution.js
+++ problems/two_sum/solution.js
@@ -4,19 +4,14 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    let table = {};
-    let answer = [];
+    const map = new Map();
     for(let i=0; i<nums.length; i++){
-        let diff = target- nums[i];
-        if(table[diff] != undefined){
-            return [table[diff], i];
+        const num = nums[i];
+        const numToLook = target - num;
+        if(map.has(numToLook)){
+            return [map.get(numToLook), i];
         }else{
-            table[nums[i]] = i;
+            map.set(num, i);
         }
     }
-    
-    //{number, index}
-    // 2 -9 = -7 -> |7| -> {2:0}
-    // 7 -9 = -2 -> |2| -> table[2] exists -> return the key of 2 and curr index.
 };
-
