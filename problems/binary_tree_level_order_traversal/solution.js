5acab2b7e0967e9d8171d87ea7cc934afb0af846
sangil <nasuler@gmail.com>
2020-10-08 00:42:30 +0000

    Sync LeetCode submission - Binary Tree Level Order Traversal (javascript)

--- problems/binary_tree_level_order_traversal/solution.js
+++ problems/binary_tree_level_order_traversal/solution.js
@@ -1,8 +1,36 @@
+/*
+Given a binary tree, return its level order traversal where the nodes in each level are ordered from left to right.
+
+Ex: Given the following tree...
+
+    4
+   / \
+  2   7
+return [[4], [2, 7]]
+Ex: Given the following tree...
+
+    2
+   / \
+  10  15
+        \
+         20
+return [[2], [10, 15], [20]]
+Ex: Given the following tree...
+
+    1
+   / \
+  9   32
+ /      \
+3        78
+return [[1], [9, 32], [3, 78]]
+*/
+
 /**
  * Definition for a binary tree node.
- * function TreeNode(val) {
- *     this.val = val;
- *     this.left = this.right = null;
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
  * }
  */
 /**
@@ -10,22 +38,27 @@
  * @return {number[][]}
  */
 var levelOrder = function(root) {
-    let lists = [];
-    helper(root, lists, 0);
-    return lists;
-};
-
-function helper(root, lists, depth){
-    if(!root) return null;
-    let list = [];
-    if(lists.length === depth){
-        lists.push(list);
-    }else{
-        list = lists[depth];
+    const level = [];
+    if(!root){
+        return level;
     }
-    list.push(root.val);
-    helper(root.left, lists, depth+1);
-    helper(root.right, lists, depth+1);
-}
-
-
+    const queue = [];
+    queue.push(root);
+    while(!!queue.length){
+        const size = queue.length
+        const currentLevel = [];
+        for(let i=0; i<size; i++){
+            const currentNode = queue.shift();
+            currentLevel.push(currentNode.val);
+            if(currentNode.left){
+                queue.push(currentNode.left);
+            }
+            if(currentNode.right){
+                queue.push(currentNode.right);
+            }
+        }
+        
+        level.push(currentLevel);        
+    }
+    return level;
+};
