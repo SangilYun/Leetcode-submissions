128fc2248eaf11d7028ceebe03a26262d6997737
sangil <nasuler@gmail.com>
2020-10-17 08:33:49 +0000

    Sync LeetCode submission - Validate Binary Search Tree (javascript)

--- problems/validate_binary_search_tree/solution.js
+++ problems/validate_binary_search_tree/solution.js
@@ -1,21 +1,58 @@
+/*
+Given a binary tree, determine if it is a valid binary search tree (BST).
+
+Assume a BST is defined as follows:
+
+The left subtree of a node contains only nodes with keys less than the node's key.
+The right subtree of a node contains only nodes with keys greater than the node's key.
+Both the left and right subtrees must also be binary search trees.
+ 
+
+Example 1:
+
+    2
+   / \
+  1   3
+
+Input: [2,1,3]
+Output: true
+Example 2:
+
+    5
+   / \
+  1   4
+     / \
+    3   6
+
+Input: [5,1,4,null,null,3,6]
+Output: false
+Explanation: The root node's value is 5 but its right child's value is 4.
+*/
+
 /**
  * Definition for a binary tree node.
- * function TreeNode(val) {
- *     this.val = val;
- *     this.left = this.right = null;
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
  * }
  */
 /**
  * @param {TreeNode} root
  * @return {boolean}
  */
-
-
-var isValidBST = function(root, upperBound=Infinity, lowerBound=-Infinity) {
-    if(!root) return true;
-    if(root.val >= upperBound || lowerBound >= root.val) return false;
-    return isValidBST(root.left, Math.min(upperBound, root.val), lowerBound)
-            &&isValidBST(root.right, upperBound, Math.max(lowerBound, root.val));
-            
+var isValidBST = function(root) {
+    return !isNotBST(root, -Infinity, Infinity)
 };
 
+const isNotBST = (root, min, max) =>{
+    if(!root){
+        return false;
+    }
+
+    if(min < root.val && root.val < max){
+        return isNotBST(root.left, min, root.val) || isNotBST(root.right, root.val, max)
+    }
+    return true;
+    
+}
