03902625f9735b99908b8000656d59ae8cc03b27
sangil <nasuler@gmail.com>
2020-10-22 07:00:55 +0000

    Sync LeetCode submission - Letter Combinations of a Phone Number (javascript)

--- problems/letter_combinations_of_a_phone_number/solution.js
+++ problems/letter_combinations_of_a_phone_number/solution.js
@@ -1,31 +1,55 @@
+/*
+This question is asked by Google. Given a string of digits, return all possible text messages those digits could send. Note: The mapping of digits to letters is as follows…
+
+0 -> null
+1 -> null
+2 -> "abc"
+3 -> "def"
+4 -> "ghi"
+5 -> "jkl"
+6 -> "mno"
+7 -> "pqrs"
+8 -> "tuv"
+9 -> "wxyz"
+Ex: digits = "23" return ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"]
+*/
+
+
 /**
  * @param {string} digits
  * @return {string[]}
  */
-const MAPPING = {
-        2:'abc',
-        3:'def',
-        4:'ghi',
-        5:'jkl',
-        6:'mno',
-        7:'pqrs',
-        8:'tuv',
-        9:'wxyz'
-    }
+
+const digitToString = {
+    0:null,
+    1:null,
+    2:"abc",
+    3:"def",
+    4:"ghi",
+    5:"jkl",
+    6:"mno",
+    7:"pqrs",
+    8:"tuv",
+    9:"wxyz"
+}
+
 var letterCombinations = function(digits) {
-    let result = [];
-    helper(result, digits, "", 0);
+    if(!digits){
+        return [];
+    }
+    const result =[];
+    helper("", digits, result, 0);
     return result;
 };
 
-function helper(result, digits, curr, index){
-    if(digits.length === index){
-        if(curr) result.push(curr);
+const helper= (str, digits, result, digitsIndex) =>{
+    if(digitsIndex >= digits.length){
+        result.push(str);
         return;
     }
-    
-    let letters = MAPPING[digits[index]];
-    for(let i=0; i<letters.length; i++){
-        helper(result, digits, curr+letters[i], index+1);
+
+    const digitStr = digitToString[digits[digitsIndex]];
+    for(let j=0; j<digitStr.length; j++){
+        helper(str+digitStr[j], digits, result, digitsIndex+1);    
     }
 }
