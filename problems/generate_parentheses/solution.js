21f245b5febd38df74a14edabc12a11194614144
sangil <nasuler@gmail.com>
2020-10-25 14:38:21 +0000

    Sync LeetCode submission - Generate Parentheses (javascript)

--- problems/generate_parentheses/solution.js
+++ problems/generate_parentheses/solution.js
@@ -1,14 +1,28 @@
+/*
+Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+
+Example 1:
+Input: n = 3
+Output: ["((()))","(()())","(())()","()(())","()()()"]
+
+
+Example 2:
+Input: n = 1
+Output: ["()"]
+*/
+
 /**
  * @param {number} n
  * @return {string[]}
  */
+
 var generateParenthesis = function(n) {
     let answer = [];
     helper(answer, n, n, [], 0)
     return answer;
 };
 
-function helper(answer, leftRem, rightRem, list, index){
+const helper = (answer, leftRem, rightRem, list, index)=>{
     if(leftRem < 0 || rightRem < leftRem) return ;
     if(leftRem === 0 && rightRem ===0) answer.push(list.join(''));
     else{
@@ -17,4 +31,5 @@ function helper(answer, leftRem, rightRem, list, index){
         list[index] = ')';
         helper(answer, leftRem, rightRem-1, list, index+1);    
     }
+    
 }
