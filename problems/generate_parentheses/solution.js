639ea5c74eb1ed318f4d4d8e7f050a9dcef830c0
sangil <nasuler@gmail.com>
2020-10-26 07:36:23 +0000

    Sync LeetCode submission - Generate Parentheses (javascript)

--- problems/generate_parentheses/solution.js
+++ problems/generate_parentheses/solution.js
@@ -17,19 +17,21 @@ Output: ["()"]
  */
 
 var generateParenthesis = function(n) {
-    let answer = [];
-    helper(answer, n, n, [], 0)
-    return answer;
+    const result = [];
+    helper(result, '', 0, 0, n);
+    return result;
 };
 
-const helper = (answer, leftRem, rightRem, list, index)=>{
-    if(leftRem < 0 || rightRem < leftRem) return ;
-    if(leftRem === 0 && rightRem ===0) answer.push(list.join(''));
-    else{
-        list[index] = '(';
-        helper(answer, leftRem-1, rightRem, list, index+1);
-        list[index] = ')';
-        helper(answer, leftRem, rightRem-1, list, index+1);    
+const helper = (result, current, open, close, n)=>{
+    if(current.length === n*2){
+        result.push(current);
+        return;
+    }
+    if(open < n){
+        helper(result, current+'(', open+1, close, n);
+    }
+    if(close< open){
+        helper(result, current+')', open, close+1,n);
     }
     
 }
