fc6d0832ae6c929fde270ef3ab803e7743cb075e
sangil <nasuler@gmail.com>
2020-10-27 08:40:24 +0000

    Sync LeetCode submission - Combination Sum (javascript)

--- problems/combination_sum/solution.js
+++ problems/combination_sum/solution.js
@@ -1,22 +1,45 @@
+/*
+ Given a list of positive numbers without duplicates and a target number, find all unique combinations of the numbers that sum to the target. 
+ Note: You may use the same number more than once.
+
+Ex: Given the following numbers and target…
+
+numbers = [2,4,6,3], target = 6,
+return [
+    [2,2,2],
+    [2,4],
+    [3,3],
+    [6]
+]
+*/
+
 /**
  * @param {number[]} candidates
  * @param {number} target
  * @return {number[][]}
  */
 var combinationSum = function(candidates, target) {
-    let answer =[];
-    let currentList = [];
-    findCombination(candidates, 0, target, answer, currentList);
-    return answer;
+    const result = [];
+    helper(candidates, target, result, [], 0);
+    return result;
 };
 
-const findCombination =(candidates, index, target, answer, currentList)=>{
-    if(target === 0) answer.push(currentList.slice());
-    if(target <0) return ;
-    
-    for(let i=index; i<candidates.length; i++){
-        currentList.push(candidates[i]);
-        findCombination(candidates, i, target-candidates[i], answer, currentList);
-        currentList.pop();
+const helper = (candidates, target, result, tempArr, start) =>{
+    if(target < 0){
+        return;
+    }
+    if(target === 0){
+        result.push(tempArr.slice()); //⭐️push a copy of tempArr⭐️
+        return;
+    }
+    for(let i=start; i<candidates.length; i++){
+        tempArr.push(candidates[i]);
+        helper(candidates, target-candidates[i], result, tempArr, i);
+        tempArr.pop()
     }
 }
+
+/*
+Runtime: O(N^T) where N is our total number of numbers and T is our target. This results from having N choices at each of our T states.
+Space complexity: O(T) where T is our target. This results from having at most T calls on our stack from our recursive calls.
+*/
