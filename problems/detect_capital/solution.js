f69af1ab4d8d709acbe6fc38163e33f59a9e1fa7
sangil <nasuler@gmail.com>
2020-11-04 13:59:44 +0000

    Sync LeetCode submission - Detect Capital (javascript)

---
+++ problems/detect_capital/solution.js
@@ -0,0 +1,7 @@
+/**
+ * @param {string} word
+ * @return {boolean}
+ */
+var detectCapitalUse = function(word) {
+    return word === word.toUpperCase() || word === word[0] + word.substr(1).toLowerCase();
+};
